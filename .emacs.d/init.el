7085e7717a224311ce1edca691e54ecd524739d8
Roi Martin <jroi.martin@gmail.com>
2023-05-20 12:27:51 +0200

    .emacs.d: add emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,59 @@
+;;; Custom file
+
+;; Set custom file location
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(load custom-file)
+
+;;; Packages
+
+;; Enable MELPA repository
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+;; expand-region
+(global-set-key (kbd "C-=") #'er/expand-region)
+
+;; ido
+(ido-mode t)
+(setq ido-enable-flex-matching t)
+(setq ido-auto-merge-work-directories-length -1)
+
+;; go-mode
+(add-hook 'go-mode-hook
+	  #'(lambda ()
+	      (eglot-ensure)
+	      (add-hook 'before-save-hook
+			#'(lambda ()
+			    (call-interactively 'eglot-code-action-organize-imports))
+			nil
+			t)
+	      (add-hook 'before-save-hook #'eglot-format-buffer nil t)))
+
+;;; User interface
+
+;; Disable tool bar
+(tool-bar-mode 0)
+
+;; Disable menu bar
+(menu-bar-mode 0)
+
+;; Disable scroll bar
+(scroll-bar-mode 0)
+
+;; Disable startup screen
+(setq inhibit-startup-screen t)
+
+;; Disable bell
+(setq ring-bell-function 'ignore)
+
+;; Show column number
+(setq column-number-mode t)
+
+;; Set a darker region face color
+(set-face-attribute 'region nil :background "#eee")
+
+;; Enable paren mode
+(show-paren-mode t)
+(setq show-paren-delay 0)
+
+;;; Keymaps
+(global-set-key (kbd "C-c t") 'ansi-term)
