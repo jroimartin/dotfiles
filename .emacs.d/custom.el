c4500242f6d890a54e3e351383cfe61a8d152f37
Roi Martin <jroi.martin@gmail.com>
2023-09-22 20:01:49 +0200

    .emacs.d: add workaround to fix seq issue with magit
    Relates to https://github.com/magit/magit/issues/5011

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -7,7 +7,7 @@
  ;; If there is more than one, they won't work right.
  '(org-agenda-files '("~/org/tasks.org"))
  '(package-selected-packages
-   '(sqlite3 forge dockerfile-mode rust-mode yaml-mode magit go-mode markdown-mode eglot)))
+   '(seq sqlite3 forge dockerfile-mode rust-mode yaml-mode magit go-mode markdown-mode eglot)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
