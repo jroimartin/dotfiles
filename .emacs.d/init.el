20a07b4660c03a97ec805a0cf7f000dc94c64be5
Roi Martin <jroi.martin@gmail.com>
2024-03-12 17:46:42 +0100

    .emacs.d: do not set split-height-threshold

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -55,9 +55,6 @@
 ;; Show column number.
 (customize-set-variable 'column-number-mode t)
 
-;; Split a window vertically only if it has at least 125 lines.
-(customize-set-variable 'split-height-threshold 125)
-
 ;;;; Faces.
 
 ;; Set a darker region face color.
