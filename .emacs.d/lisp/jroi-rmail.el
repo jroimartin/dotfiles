4460539caff168b6151a4a0e07f58a8724f81d3f
Roi Martin <jroi.martin@gmail.com>
2024-10-01 23:28:30 +0200

    .emacs.d/lisp/jroi-rmail.el: fix doc style issue

--- .emacs.d/lisp/jroi-rmail.el
+++ .emacs.d/lisp/jroi-rmail.el
@@ -1,7 +1,7 @@
 ;;; Rmail utils.
 
 (defun jroi-rmail-url (url)
-  "Run `rmail' on URL."
+  "Run Rmail on URL."
   (interactive "sRun rmail on URL: ")
   (let ((filename (make-temp-file "url" nil (url-file-extension url))))
     (url-copy-file url filename 'ok-if-already-exists)
