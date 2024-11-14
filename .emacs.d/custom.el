e03e6bcc97bd5256c38dcaa865d5c68b0a1d159b
Roi Martin <jroi.martin@gmail.com>
2024-11-14 20:28:24 +0100

    .emacs.d: replace geiser-racket with racket-mode

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -6,7 +6,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(geiser-chez tmr geiser-racket elfeed htmlize geiser-guile denote forge magit markdown-mode)))
+   '(racket-mode geiser-chez tmr elfeed htmlize geiser-guile denote forge magit markdown-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
