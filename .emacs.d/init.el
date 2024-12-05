5322c2103a8eb14e1f254caa31799987c4708418
Roi Martin <jroi.martin@gmail.com>
2024-12-05 15:28:39 +0100

    .emacs.d/init.el: update tree-sitter language grammars

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -45,13 +45,13 @@
 ;; Add configuration for downloading and installing tree-sitter
 ;; language grammars.
 (setq treesit-language-source-alist
-      '((go . ("https://github.com/tree-sitter/tree-sitter-go.git" "v0.23.1"))
-	(gomod . ("https://github.com/camdencheek/tree-sitter-go-mod.git" "v1.0.2"))
-	(rust . ("https://github.com/tree-sitter/tree-sitter-rust.git" "v0.23.0"))
+      '((go . ("https://github.com/tree-sitter/tree-sitter-go.git" "v0.23.4"))
+	(gomod . ("https://github.com/camdencheek/tree-sitter-go-mod.git" "v1.1.0"))
+	(rust . ("https://github.com/tree-sitter/tree-sitter-rust.git" "v0.23.2"))
 	(dockerfile . ("https://github.com/camdencheek/tree-sitter-dockerfile.git" "v0.2.0"))
 	(yaml . ("https://github.com/ikatyang/tree-sitter-yaml.git" "v0.5.0"))
-	(c . ("https://github.com/tree-sitter/tree-sitter-c.git" "v0.23.0"))
-	(cpp . ("https://github.com/tree-sitter/tree-sitter-cpp.git" "v0.23.0"))))
+	(c . ("https://github.com/tree-sitter/tree-sitter-c.git" "v0.23.2"))
+	(cpp . ("https://github.com/tree-sitter/tree-sitter-cpp.git" "v0.23.4"))))
 
 ;; Install missing grammars.
 (mapc #'(lambda (lang)
