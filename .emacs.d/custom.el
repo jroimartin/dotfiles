5e940de01932ac2203e44ae826a449cea1df8724
Roi Martin <jroi.martin@gmail.com>
2025-01-28 17:49:01 +0100

    .emacs.d: remove elfeed

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -6,7 +6,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(zig-mode racket-mode geiser-chez tmr elfeed htmlize geiser-guile denote forge magit markdown-mode)))
+   '(zig-mode racket-mode geiser-chez tmr htmlize geiser-guile denote forge magit markdown-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
